770205f9376bdf6c0e109287878868228ff92513
Jean-César <jeancesar.bazin@gmail.com>
2021-08-31 15:18:04 +0200

    update : updating controller

--- src/main/java/tourGuide/controller/UserController.java
+++ src/main/java/tourGuide/controller/UserController.java
@@ -2,19 +2,30 @@ package tourGuide.controller;
 
 import com.jsoniter.output.JsonStream;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import tourGuide.consumer.GpsGateway;
+import tourGuide.consumer.RewardGateway;
+import tourGuide.model.User;
 import tourGuide.model.VisitedLocation;
 import tourGuide.service.TourGuideService;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("user")
 public class UserController {
 
 
     @Autowired
     TourGuideService tourGuideService;
 
+    @Autowired
+    GpsGateway gpsGateway;
+    @Autowired
+    RewardGateway rewardGateway;
 
     @RequestMapping("/getLocation")
     public String getLocation(@RequestParam String userName) {
@@ -28,6 +39,14 @@ public class UserController {
     @RequestMapping("/getRewards")
     public String getRewards(@RequestParam String userName) {
         return JsonStream.serialize(tourGuideService.getUserRewards(userName));
+    }
+
+    @RequestMapping("/users")
+    public List<User> getUsers() {
+        TourGuideService tourGuideService = new TourGuideService(gpsGateway, rewardGateway);
+        List<User> users = tourGuideService.getAllUsers();
+        tourGuideService.tracker.stopTracking();
+        return users;
     }
 
 
