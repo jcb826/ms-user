a335ad0253f009ea3eb37cb6da4c08de3a6d7d40
Jean-César <jeancesar.bazin@gmail.com>
2021-11-02 14:39:16 +0100

    ending 50 % jacoco test coverage

--- src/test/java/tourGuide/msuser/MsUserApplicationIT.java
+++ src/test/java/tourGuide/msuser/MsUserApplicationIT.java
@@ -96,7 +96,7 @@ class MsUserApplicationIT {
     public void getNearbyAttractions() throws InterruptedException {
         Locale.setDefault(new Locale("en", "US"));
 
-        InternalTestHelper.setInternalUserNumber(0);
+        InternalTestHelper.setInternalUserNumber(1);
         TourGuideService tourGuideService = new TourGuideService(gpsGateway, rewardGateway);
 
         User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
@@ -111,25 +111,7 @@ class MsUserApplicationIT {
 
         Assertions.assertEquals(5, attractions.length);
     }
-/*
-    public void getNearbyAttractions() {
-        Locale.setDefault(new Locale("en", "US"));
-        GpsUtil gpsUtil = new GpsUtil();
-        RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-        InternalTestHelper.setInternalUserNumber(0);
-        TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 
-        User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
-        VisitedLocation visitedLocation = tourGuideService.trackUserLocation(user);
-
-        List<Attraction> attractions = tourGuideService.getNearByAttractions(visitedLocation);
-
-        tourGuideService.tracker.stopTracking();
-
-        assertEquals(5, attractions.size());
-    }
-
- */
 
 }
 
